2f556e6d66c5f3e9d77b6179cc1bb6ee2f102bd0
arnaubalart <arnaubalart@yahoo.com>
2021-10-25 18:22:25 +0200

    Backend hecho

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
 //A, B, C,..,G deben de ser chistes
-const jokes = ["A", "B", "C", "D", "E", "F", "G"];
+const jokes = ["Pablo, ¿qué planeta va después de Marte? -Pues miércole!", "¿Dónde va el Jorge? -@!", "-Hola, ¿tiene comida de vegano? -Clago, y de inviegno, este es un gestaugante fgancés!", "-Un ratón le dice a una rata: ¿Qué haces ahí sentada? -Estoy esperando un ratito", "-¿Y cómo quiere la señora los huevos? -En la barbilla, si puede ser", "¿Cómo se dice pelo sucio en chino? - Chin cham pu", "¿Cuál es el colmo de Aladdín? -Tener mal genio"];
 
 function getJoke() {
     document.getElementById("joke").innerHTML = '"' + jokes[parseInt(Math.random() * jokes.length)] + '"';
